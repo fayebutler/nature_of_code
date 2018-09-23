a94b573b6c48c97cf1991675e74a66c65251d29f
Faye Butler <faye.alexandra.butler1@gmail.com>
2018-09-23 17:43:41 +0100

    altering to use cairo and imageio

--- randomwalker.py
+++ randomwalker.py
@@ -1,13 +1,72 @@
-
 #!/usr/bin/env python
 
 import cairocffi as cairo
+import random
+from numpy import pi
+import imageio
+
+# with imageio.get_writer('/path/to/movie.gif', mode='I') as writer:
+#     for filename in filenames:
+#         image = imageio.imread(filename)
+#         writer.append_data(image)
+
+class Walker:
+
+	def __init__(self, width, height):
+		self.surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, width, height)
+		self.context = cairo.Context(self.surface)
+		self.context.set_source_rgb(1, 1, 1)
+		self.context.paint()
+
+		self.writer = imageio.get_writer('animated.gif', mode='I')
+
+		self.radius = 5
+		self.x = 10
+		self.y = 10
+		self.num = 1;
+
+	def circle(self,x,y,r,fill=False):
+		self.context.set_source_rgb(0, 0, 0)
+		self.context.arc(x,y,r,0,(pi*2))
+		if fill:
+		  self.context.fill()
+		else:
+		  self.context.stroke()
 
 
+	def draw(self):
+		self.circle(self.x, self.y, self.radius, fill=True)
+		print('img_{}.png'.format(self.num))
+		self.surface.write_to_png('img_{}.png'.format(self.num))
+		image = imageio.imread('img_{}.png'.format(self.num))
+		self.writer.append_data(image)
+		self.num += 1
 
-# from PIL import Image, ImageDraw
-# import random
-#
+
+	def step(self):
+		random_num = random.random()
+
+		if random_num < 0.4 :
+			#high probability it moves down
+			#take a step down
+			self.y += self.radius * 2
+		elif random_num < 0.6 :
+			#take a step right
+			self.x += self.radius * 2
+		elif random_num < 0.8 :
+			#take a step left
+			self.x -= self.radius * 2
+		elif random_num <= 1 :
+			#take a step up
+			self.y -= self.radius * 2
+
+		self.draw()
+
+
+walker = Walker(200, 200)
+for i in range(0, 10):
+	walker.step()
+
 # size = (500,500)
 # white = (255,255,255)
 # black = (0,0,0)
