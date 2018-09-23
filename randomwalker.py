ce5188bc1e0d7811301dae88394e9c53b905c007
Faye Butler <faye.alexandra.butler1@gmail.com>
2018-09-23 18:07:51 +0100

    adding position check

--- randomwalker.py
+++ randomwalker.py
@@ -5,10 +5,6 @@ import random
 from numpy import pi
 import imageio
 
-# with imageio.get_writer('/path/to/movie.gif', mode='I') as writer:
-#     for filename in filenames:
-#         image = imageio.imread(filename)
-#         writer.append_data(image)
 
 class Walker:
 
@@ -21,11 +17,12 @@ class Walker:
 		self.writer = imageio.get_writer('animated.gif', mode='I')
 
 		self.radius = 5
-		self.x = 10
-		self.y = 10
+		self.x = width/2.0
+		self.y = height/2.0
 		self.num = 1;
+		self.positions = []
 
-	def circle(self,x,y,r,fill=False):
+	def circle(self, x, y, r, fill=False):
 		self.context.set_source_rgb(0, 0, 0)
 		self.context.arc(x,y,r,0,(pi*2))
 		if fill:
@@ -44,94 +41,29 @@ class Walker:
 
 
 	def step(self):
-		random_num = random.random()
 
-		if random_num < 0.4 :
-			#high probability it moves down
-			#take a step down
-			self.y += self.radius * 2
-		elif random_num < 0.6 :
-			#take a step right
-			self.x += self.radius * 2
-		elif random_num < 0.8 :
-			#take a step left
-			self.x -= self.radius * 2
-		elif random_num <= 1 :
-			#take a step up
-			self.y -= self.radius * 2
+		while (self.x, self.y) in self.positions:
+			print(" ({}, {}) is in positions".format(self.x, self.y))
+			random_num = random.random()
 
+			if random_num < 0.4 :
+				#high probability it moves down
+				#take a step down
+				self.y += self.radius * 2
+			elif random_num < 0.6 :
+				#take a step right
+				self.x += self.radius * 2
+			elif random_num < 0.8 :
+				#take a step left
+				self.x -= self.radius * 2
+			elif random_num <= 1 :
+				#take a step up
+				self.y -= self.radius * 2
+
+		self.positions.append((self.x, self.y))
 		self.draw()
 
 
 walker = Walker(200, 200)
 for i in range(0, 10):
 	walker.step()
-
-# size = (500,500)
-# white = (255,255,255)
-# black = (0,0,0)
-#
-# class Walker(object):
-# 	"""
-# 	"""
-#
-# 	def __init__(self, width, height):
-# 		self.x = width/2.0
-# 		self.y = height/2.0
-# 		self.width = 2
-# 		self.height = 2
-# 		self.num = 1;
-#
-# 		self.first_img = Image.new("RGB", size, white)
-# 		self.prev_img = self.first_img
-# 		self.first_img = self.draw()
-# 		self.imgs = [self.first_img]
-#
-# 	def draw(self):
-# 		img = self.prev_img.copy()
-# 		draw = ImageDraw.Draw(img)
-# 		position = [(self.x - self.width), (self.y - self.height), (self.x + self.width), (self.y + self.height)]
-# 		draw.ellipse(position, black, black)
-# 		#img.save("walk" + str(self.num) + ".jpg", "JPEG")
-# 		return img
-#
-# 	def step(self):
-# 		#random_num = random.randrange(4)
-# 		random_num = random.random()
-# 		print("random number", random_num)
-# 		self.num += 1
-# 		if random_num < 0.4 :
-# 			#high probability it moves down
-# 			#take a step down
-# 			self.y += self.height * 2
-# 			img = self.draw()
-# 			self.imgs.append(img)
-# 			self.prev_img = img
-# 		elif random_num < 0.6 :
-# 			#take a step right
-# 			self.x += self.width * 2
-# 			img = self.draw()
-# 			self.imgs.append(img)
-# 			self.prev_img = img
-# 		elif random_num < 0.8 :
-# 			#take a step left
-# 			self.x -= self.width * 2
-# 			img = self.draw()
-# 			self.imgs.append(img)
-# 			self.prev_img = img
-# 		elif random_num <= 1 :
-# 			#take a step up
-# 			self.y -= self.height * 2
-# 			img = self.draw()
-# 			self.imgs.append(img)
-# 			self.prev_img = img
-#
-# #initial position = [(self.x - self.width), (self.y - self.height), (self.x + self.width), (self.y + self.height)]
-#
-# walker = Walker(500, 500)
-# for i in range(0, 100):
-# 	walker.step()
-# print("number of images", len(walker.imgs))
-#
-# im = walker.imgs[0]
-# im.save("walker.gif", save_all=True, append_images=walker.imgs, duration=300)
